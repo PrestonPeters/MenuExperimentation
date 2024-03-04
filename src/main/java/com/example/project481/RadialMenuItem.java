ee8e11ed3e26cba496ab689ee23b32d5227364f0
Preston Peters <ptp726@usask.ca>
2024-03-04 17:08:46 -0600

    Updated the hit detection to account for the presence of a base item.

--- src/main/java/com/example/project481/RadialMenuItem.java
+++ src/main/java/com/example/project481/RadialMenuItem.java
@@ -1,7 +1,9 @@
 package com.example.project481;
 
 public class RadialMenuItem extends MenuItem {
+    private boolean isBaseItem;             // Bool indicating whether this is the central item
     private int index;
+    private double baseItemRadius = 50;     // Currently hardcoded at 50
     private double radius;
     private double originX, originY;
 
@@ -15,6 +17,8 @@ public class RadialMenuItem extends MenuItem {
 
     @Override
     public boolean contains(double x, double y) {
+        if (isBaseItem) return baseItemContains(x, y);
+
         double xDifference = x - originX;
         double yDifference = y - originY;
 
@@ -26,5 +30,12 @@ public class RadialMenuItem extends MenuItem {
         angle = 360 - angle;
         int partition = (int) Math.floor(angle / (double) (360 / 8));
         return partition == index;
+    }
+
+    public boolean baseItemContains(double x, double y) {
+        double xDifference = x - originX;
+        double yDifference = y - originY;
+
+        return Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2)) <= baseItemRadius;
     }
 }
