64e442792917abdbd82541e53140bde224d18e40
Cassidy Klassen <ccj670@usask.ca>
2024-03-04 10:38:45 -0600

    Hookup pubsub, methods to adjust menuMode

---
+++ src/main/java/com/example/project481/MenuItem.java
@@ -0,0 +1,14 @@
+package com.example.project481;
+
+public class MenuItem {
+    String text;
+
+    public MenuItem(String name){
+        this.text = name;
+    }
+
+    public boolean contains(double x, double y) {
+        return false;
+    }
+
+}
--- src/main/java/com/example/project481/View.java
+++ src/main/java/com/example/project481/View.java
@@ -2,7 +2,11 @@ package com.example.project481;
 
 import javafx.scene.layout.StackPane;
 
+import java.util.ArrayList;
+
 public class View extends StackPane implements Subscriber {
+    Controller.MenuMode menuMode;
+    ArrayList<MenuItem> menuItems;
     public View() {
         setFocusTraversable(true);
     }
@@ -14,7 +18,28 @@ public class View extends StackPane implements Subscriber {
         this.setOnMouseMoved(controller::handleMouseMoved);
     }
 
-    public void draw(){}
+    public void draw(){
+        switch (menuMode){
+            case LINEAR -> {}
+            case RADIAL -> {}
+            case GRID -> {}
+            case SCROLL -> {}
+            case NONE -> {}
+        }
+    }
 
-    public void receiveNotification(String channel, Object message){}
+    public void receiveNotification(String channel, Object message){
+        switch (channel){
+            case "menuMode" -> {
+                // when menu mode changes
+                this.menuMode = (Controller.MenuMode) message;
+                draw();
+            }
+            case "menuItems" -> {
+                // when menuItem list changes
+                this.menuItems = (ArrayList<MenuItem>) message;
+                draw();
+            }
+        }
+    }
 }
