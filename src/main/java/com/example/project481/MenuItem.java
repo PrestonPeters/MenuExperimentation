b57c5d9128484fc9646dbb614f7db7cbe911918e
Preston Peters <preston.t.peters@gmail.com>
2024-03-21 10:20:26 -0600

    Initial refactoring to set up the system to allow for traversal of previous menus and submenus.

--- src/main/java/com/example/project481/MenuItem.java
+++ src/main/java/com/example/project481/MenuItem.java
@@ -3,12 +3,14 @@ package com.example.project481;
 public class MenuItem {
     private boolean isBaseItem;
     private String text;
+    private Menu previousMenu;
     private Menu subMenu;
     protected double x, y;
 
     public MenuItem(String text, boolean isBaseItem) {
         this.isBaseItem = isBaseItem;
         this.text = text;
+        this.previousMenu = null;
         this.subMenu = null;
     }
 
@@ -22,9 +24,13 @@ public class MenuItem {
 
     public void setSubMenu(Menu subMenu) { this.subMenu = subMenu; }
 
+    public boolean hasPreviousMenu() { return previousMenu != null; }
+
+    public void setPreviousMenu(Menu previousMenu) { this.previousMenu = previousMenu; }
+
     public boolean isBaseItem() { return isBaseItem; }
     public boolean contains(double x, double y) {
-        // STUB
+        // STUB TO BE OVERRIDDEN
         return false;
     }
 
